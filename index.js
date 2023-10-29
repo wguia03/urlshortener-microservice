e5d9a2e710fd6ae300418a13f1f60768ce75ab04
WillGM2003 <willgm.2003@gmail.com>
2023-10-28 22:21:27 -0500

    added middleware in api/shorturl

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dns = require("dns");
+const urlRegex = require("url-regex");
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// Middleware
+app.post("/api/shorturl", (req, res, next) => {
+  const { url } = req.body;
+
+  if (!urlRegex({ exact: true }).test(url)) {
+    return res.status(400).json({ error: "Invalid URL format" });
+  }
+
+  const urlObject = new URL(url);
+  const host = urlObject.hostname;
+
+  dns.lookup(host, (err) => {
+    if (err) {
+      res.status(400).json({ error: "Invalid hostname in URL" });
+    } else {
+      next();
+    }
+  });
 });
 
-app.listen(port, function() {
+// Endpoints
+app.get("/api/hello", function (req, res) {
+  res.json({ message: "Hello API" });
+});
+
+app.post("/api/shorturl", (req, res) => {
+  res.json({ message: "Succesfully operation" });
+});
+
+// Listener
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
