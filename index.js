801f2b34ecaeeb244fad0b7fb851d4978f7bc586
WillGM2003 <willgm.2003@gmail.com>
2023-11-01 01:00:02 -0500

    added db connection and redirect to original url

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dns = require("dns");
 const validUrl = require("valid-url");
-const perma = require("perma"); // in testing
+const perma = require("perma");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -22,27 +23,19 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Middleware
-/*
-app.post("/api/shorturl", (req, res, next) => {
-  const { url } = req.body;
-
-  if (!validUrl.isWebUri(url)) {
-    return res.json({ error: "invalid url" });
-  }
-
-  const urlObject = new URL(url);
-  const host = urlObject.hostname;
-
-  dns.lookup(host, (err) => {
-    if (err) {
-      return res.json({ error: "invalid url" });
-    } else {
-      next();
-    }
-  });
+// Database connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-*/
+
+// Model
+const urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: String,
+});
+
+const urlModel = mongoose.model("urlModel", urlSchema);
 
 // Endpoints
 app.get("/api/hello", function (req, res) {
@@ -63,12 +56,58 @@ app.post("/api/shorturl", (req, res) => {
     if (err) return res.json({ error: "invalid url" });
   });
 
-  const shortUrl = perma(url, 5);
-  res.json({ original_url: url, short_url: shortUrl });
+  urlModel
+    .findOne({ original_url: url })
+    .then((urlFound) => {
+      if (urlFound) {
+        console.log("URL already added to db");
+        res.json({
+          original_url: urlFound.original_url,
+          short_url: urlFound.short_url,
+        });
+      } else {
+        const shortUrl = perma(url, 5);
+
+        const new_url = new urlModel({
+          original_url: url,
+          short_url: shortUrl,
+        });
+
+        new_url
+          .save()
+          .then((doc) => {
+            console.log(doc);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+
+        res.json({ original_url: url, short_url: shortUrl });
+      }
+    })
+    .catch((err) => {
+      res.json({ error: "Failed database connection" });
+    });
 });
 
 app.get("/api/shorturl/:short_url", (req, res) => {
-  res.json({ message: "In progess" });
+  const short_url = req.params.short_url;
+  let original_url;
+
+  urlModel
+    .findOne({
+      short_url: short_url,
+    })
+    .then((doc) => {
+      if (doc) {
+        res.redirect(doc.original_url);
+      } else {
+        res.json({ error: "URL does not exist" });
+      }
+    })
+    .catch((err) => {
+      res.json({ error: "Failed database connection" });
+    });
 });
 
 // Listener
